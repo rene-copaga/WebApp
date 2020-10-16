ebd6787f4915caec37db35ed8242a1916d9922a4
Rene Copaga <rene.copaga@gmail.com>
2020-10-16 15:12:17 -0400

    Configuring Entity Framework Core Services and Middleware

--- Startup.cs
+++ Startup.cs
@@ -5,27 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApp.Models;
 
 namespace WebApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration config)
         {
+            Configuration = config;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public IConfiguration Configuration { get; set; }
 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opts => {
+                opts.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, DataContext context)
+        {
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -35,6 +42,8 @@ namespace WebApp
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            SeedData.SeedDatabase(context);
         }
     }
 }
