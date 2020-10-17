47b4f0da7446f4987586666a698520c549123380
Rene Copaga <rene.copaga@gmail.com>
2020-10-17 15:15:52 -0400

    Using Asynchronous Actions

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -17,38 +18,36 @@ namespace WebApp.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetProducts()
+        public IAsyncEnumerable<Product> GetProducts()
         {
             return context.Products;
         }
 
         [HttpGet("{id}")]
-        public Product GetProduct(long id, 
-                [FromServices] ILogger<ProductsController> logger)
+        public async Task<Product> GetProduct(long id)
         {
-            logger.LogDebug("GetProduct Action Invoked");
-            return context.Products.Find(id);
+            return await context.Products.FindAsync(id);
         }
 
         [HttpPost]
-        public void SaveProduct([FromBody] Product product)
+        public async Task SaveProduct([FromBody] Product product)
         {
-            context.Products.Add(product);
-            context.SaveChanges();
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
         }
 
         [HttpPut]
-        public void UpdateProduct([FromBody] Product product)
+        public async Task UpdateProduct([FromBody] Product product)
         {
             context.Products.Update(product);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
         }
 
         [HttpDelete("{id}")]
-        public void DeleteProduct(long id)
+        public async Task DeleteProduct(long id)
         {
             context.Products.Remove(new Product() { ProductId = id });
-            context.SaveChanges();
+            await context.SaveChangesAsync();
         }
     }
 }
