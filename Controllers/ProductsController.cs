ef477592826265a7badf7a27105b0272e4bbd722
Rene Copaga <rene.copaga@gmail.com>
2020-10-17 14:40:06 -0400

    Model Binding from the Request Body

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -29,5 +29,12 @@ namespace WebApp.Controllers
             logger.LogDebug("GetProduct Action Invoked");
             return context.Products.Find(id);
         }
+
+        [HttpPost]
+        public void SaveProduct([FromBody] Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
     }
 }
