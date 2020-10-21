d872b14ff4c08cff653afaeb18075ef571fa2855
Rene Copaga <rene.copaga@gmail.com>
2020-10-20 22:49:41 -0400

    Understanding the Default Content Policy

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ContentController
+    {
+        private DataContext context;
+
+        public ContentController(DataContext dataContext)
+        {
+            context = dataContext;
+        }
+
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public async Task<Product> GetObject()
+        {
+            return await context.Products.FirstAsync();
+        }
+    }
+}
