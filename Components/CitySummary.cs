c963cf82ad2190e294bf5588933397259e6a3c25
Rene Copaga <rene.copaga@gmail.com>
2020-10-26 22:39:07 -0400

    Getting Context Data

--- Components/CitySummary.cs
+++ Components/CitySummary.cs
@@ -12,10 +12,16 @@ namespace WebApp.Components
         {
             data = cdata;
         }
-        public IViewComponentResult Invoke()
+        public string Invoke()
         {
-            return new HtmlContentViewComponentResult(
-                new HtmlString("This is a <h3><i>string</i></h3>"));
+            if (RouteData.Values["controller"] != null)
+            {
+                return "Controller Request";
+            }
+            else
+            {
+                return "Razor Page Request";
+            }
         }
     }
 }
