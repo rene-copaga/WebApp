01e59d36f6d6e19a35d31d54374c22b95444eb9f
Rene Copaga <rene.copaga@gmail.com>
2020-10-29 10:34:23 -0400

    Creating Elements Programmatically

--- TagHelpers/TableHeadTagHelper.cs
+++ TagHelpers/TableHeadTagHelper.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Threading.Tasks;
 
 namespace WebApp.TagHelpers
@@ -16,9 +17,17 @@ namespace WebApp.TagHelpers
             output.TagMode = TagMode.StartTagAndEndTag;
             output.Attributes.SetAttribute("class",
                 $"bg-{BgColor} text-white text-center");
+
             string content = (await output.GetChildContentAsync()).GetContent();
-            output.Content
-                .SetHtmlContent($"<tr><th colspan=\"2\">{content}</th></tr>");
+
+            TagBuilder header = new TagBuilder("th");
+            header.Attributes["colspan"] = "2";
+            header.InnerHtml.Append(content);
+
+            TagBuilder row = new TagBuilder("tr");
+            row.InnerHtml.AppendHtml(header);
+
+            output.Content.SetHtmlContent(row);
         }
     }
 }
