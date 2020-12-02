b4b9c9ff47edcff741dcba238607ae35d4fc0fb7
Rene Copaga <rene.copaga@gmail.com>
2020-12-02 14:58:37 -0400

    Using Request Bodies as Binding Sources

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -45,5 +45,12 @@ namespace WebApp.Controllers
         {
             return $"Header: {accept}";
         }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public Product Body([FromBody] Product model)
+        {
+            return model;
+        }
     }
 }
