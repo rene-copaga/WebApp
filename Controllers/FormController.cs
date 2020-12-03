5af4d3a1f34df1efe62a7726eb9a300d4cb38ec1
Rene Copaga <rene.copaga@gmail.com>
2020-12-03 11:06:12 -0400

    Displaying Model-Level Messages

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -37,6 +37,14 @@ namespace WebApp.Controllers
                 ModelState.AddModelError(nameof(Product.Price),
                 "Enter a positive price");
             }
+            if (ModelState.GetValidationState(nameof(Product.Name))
+                    == ModelValidationState.Valid
+                    && ModelState.GetValidationState(nameof(Product.Price))
+                    == ModelValidationState.Valid
+                && product.Name.ToLower().StartsWith("small") && product.Price > 100)
+            {
+                ModelState.AddModelError("", "Small products cannot cost more than $100");
+            }
             if (!context.Categories.Any(c => c.CategoryId == product.CategoryId))
             {
                 ModelState.AddModelError(nameof(Product.CategoryId),
